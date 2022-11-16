5a0b84a931f2b919c03fcf1247f33af3a2ff1c5b
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-11-16 22:19:41 +0000

    links

--- pages/contactos.js
+++ pages/contactos.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Header from '../components/Header'
 import { request } from "../lib/datocms"
-import { SOUNDS_QUERY } from '../lib/queries'
+import { SOUNDS_QUERY, SOCIALS_QUERY } from '../lib/queries'
 
 export async function getStaticProps() {
 
@@ -9,9 +9,14 @@ export async function getStaticProps() {
         query: SOUNDS_QUERY,
     })
 
+    const socials = await request({
+        query: SOCIALS_QUERY,
+    })
+
     return {
         props: {
-        sounds: sounds.home.sons
+        sounds: sounds.home.sons,
+        socials: socials.social
         },
     }
 }
@@ -19,7 +24,7 @@ export default function Contactos({sounds}) {
 
     return (
         <>
-        <Header sounds={sounds}/>
+        <Header sounds={sounds} socials={socials}/>
         <div className="pl-20 pt-28 pr-5 md:pt-0 md:h-screen w-full flex items-center justify-center">
             <form className="space-y-5 max-w-4xl 2xl:w-[45%] 2xl:text-xl">
                 <div className="flex space-x-5">
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { request } from "../lib/datocms"
-import { HOME_QUERY, SOUNDS_QUERY } from '../lib/queries'
+import { HOME_QUERY, SOUNDS_QUERY, SOCIALS_QUERY } from '../lib/queries'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Header from '../components/Header'
@@ -16,16 +16,21 @@ export async function getStaticProps() {
       query: SOUNDS_QUERY,
   })
 
+  const socials = await request({
+      query: SOCIALS_QUERY,
+  })
+
 
   return {
       props: {
       data: data.home,
-      sounds: sounds.home.sons
+      sounds: sounds.home.sons, 
+      socials: socials.social
       },
   }
 }
 
-export default function Home({data, sounds}) {
+export default function Home({data, sounds, socials}) {
   const images = data.imagensBackground.map((imagem) => imagem.url)
   const [currentImageIndex, setCurrentImageIndex] = useState()
   const changeImage = () => {
@@ -35,7 +40,7 @@ export default function Home({data, sounds}) {
   useEffect(() => changeImage(), [])
   return (
     <>
-      <Header sounds={sounds}/>
+      <Header sounds={sounds} socials={socials}/>
       <Link href='/projetos'><Image id='bgImage' src={images[currentImageIndex]} alt="Background" layout='fill' objectFit='cover'/></Link>
     </>
   )
