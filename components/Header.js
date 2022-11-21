f0d8bf5a53a1eccc9129eb91bd90ceb3f2f2e73a
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-11-21 13:32:41 +0000

    eng

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,8 @@ import { useRef, useState, useEffect } from "react"
 import {RiMenuLine, RiCloseLine, RiInstagramFill, RiYoutubeFill} from 'react-icons/ri'
 import { AiFillFacebook} from 'react-icons/ai'
 
-
-
 const Header = ({sounds, socials}) => {
+    const { locale, locales, asPath } = useRouter();
     const allAudios = []
     const audioLinks = sounds.map((sound) => (allAudios.push(sound.url)))
     const router = useRouter()
@@ -52,8 +51,8 @@ const Header = ({sounds, socials}) => {
     return (
         <>
             <div className="font-apoc flex space-x-4 p-4 2xl:p-8 absolute z-10 right-0 2xl:text-xl">
-                <button className={`underline-offset-4 ouvir ${isPlaying ? 'underline' : ''}`} onClick={audioStart}>ouvir</button>
-                <button onClick={audioStop} className={`silencio underline-offset-4 ${isPlaying ? '' : 'underline'}`}>silêncio</button>
+                <button className={`underline-offset-4 ouvir ${isPlaying ? 'underline' : ''}`} onClick={audioStart}>{locale === 'pt' ? 'ouvir' : 'listen'}</button>
+                <button onClick={audioStop} className={`silencio underline-offset-4 ${isPlaying ? '' : 'underline'}`}>{locale === 'pt' ? 'silêncio' : 'silence'}</button>
             </div>
             <div className='fixed bottom-5 right-5 flex justify-start items-center z-10 space-x-3'>
                 <a href={socials.instagram}  target='blank' rel='noreferrer' className="block py-1 pr-1 pl-3 md:p-0"><RiInstagramFill/></a>
@@ -70,10 +69,10 @@ const Header = ({sounds, socials}) => {
                         <a><span className="self-center text-4xl md:text-2xl 2xl:text-3xl ml-16 md:ml-4 font-apoc whitespace-nowrap 3xl:text-4xl">Fernando Mota</span></a>
                     </Link>
                     <div className="hidden md:flex items-center space-x-4">  
-                        <a href="#" className="text-xs 2xl:text-lg hidden md:block ml-3 py-1 pr-4 pl-3 md:p-0 rotate-90 3xl:text-2xl">EN</a>
-                            <div className={`hidden md:block hover:underline hover:text-yellow-100 ${router.pathname == '/projetos' ? 'underline text-yellow-100' : ''}`}><Link href='/projetos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">Projetos</a></Link></div>
-                            <div className={`hidden md:block hover:underline hover:text-yellow-100`}><a href='https://instrumentariapoetica.bandcamp.com' rel="noreferrer" target='_blank' className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">Música</a></div>
-                            <div className={`hidden md:block hover:underline hover:text-yellow-100 ${router.pathname == '/contactos' ? 'underline text-yellow-100' : ''}`}><Link href='/contactos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">Contactos</a></Link></div>
+                            {locale === 'pt' ? <Link href={asPath} locale={'en'}><a className="text-xs 2xl:text-lg hidden md:block ml-3 py-1 pr-4 pl-3 md:p-0 rotate-90 3xl:text-2xl">EN</a></Link> : <Link href={asPath} locale={'pt'}><a className="text-xs 2xl:text-lg hidden md:block ml-3 py-1 pr-4 pl-3 md:p-0 rotate-90 3xl:text-2xl">PT</a></Link>}
+                            <div className={`hidden md:block hover:underline hover:text-yellow-100 ${router.pathname == '/projetos' ? 'underline text-yellow-100' : ''}`}><Link href='/projetos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">{locale === 'pt' ? 'Projectos' : 'Projects'}</a></Link></div>
+                            <div className={`hidden md:block hover:underline hover:text-yellow-100`}><a href='https://instrumentariapoetica.bandcamp.com' rel="noreferrer" target='_blank' className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">{locale === 'pt' ? 'Música' : 'Music'}</a></div>
+                            <div className={`hidden md:block hover:underline hover:text-yellow-100 ${router.pathname == '/contactos' ? 'underline text-yellow-100' : ''}`}><Link href='/contactos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">{locale === 'pt' ? 'Contactos' : 'Contacts'}</a></Link></div>
                             <div className={`hidden md:block hover:underline hover:text-yellow-100 ${router.pathname == '/bio' ? 'underline text-yellow-100' : ''}`}><Link href='/bio'><a className="ml-3 py-1 pr-4 pl-3 md:p-0 2xl:text-xl 3xl:text-2xl">Bio</a></Link></div>
                     </div>  
                 </div>
@@ -86,12 +85,12 @@ const Header = ({sounds, socials}) => {
                         <RiCloseLine/>
                     </button>
                     <div className="text-4xl pl-4 mb-4" id="close-btn"><i className="ri-close-fill"></i></div>
-                        <div className={`${router.pathname == '/projetos' ? 'underline text-yellow-100' : ''}`}><Link href='/projetos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0">Projetos</a></Link></div>
-                        <div><a href='https://instrumentariapoetica.bandcamp.com' rel="noreferrer" target='_blank' className="ml-3 py-1 pr-4 pl-3 md:p-0">Música</a></div>
-                        <div className={`${router.pathname == '/contactos' ? 'underline text-yellow-100' : ''}`}><Link href='/contactos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0">Contactos</a></Link></div>
+                        <div className={`${router.pathname == '/projetos' ? 'underline text-yellow-100' : ''}`}><Link href='/projetos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0">{locale === 'pt' ? 'Projectos' : 'Projects'}</a></Link></div>
+                        <div><a href='https://instrumentariapoetica.bandcamp.com' rel="noreferrer" target='_blank' className="ml-3 py-1 pr-4 pl-3 md:p-0">{locale === 'pt' ? 'Música' : 'Music'}</a></div>
+                        <div className={`${router.pathname == '/contactos' ? 'underline text-yellow-100' : ''}`}><Link href='/contactos'><a className="ml-3 py-1 pr-4 pl-3 md:p-0">{locale === 'pt' ? 'Contactos' : 'Contacts'}</a></Link></div>
                         <div className={`${router.pathname == '/bio' ? 'underline text-yellow-100' : ''}`}><Link href='/bio'><a className="ml-3 py-1 pr-4 pl-3 md:p-0">Bio</a></Link></div>
                     <div className="py-5 text-base">
-                        <a href="#" className="block ml-3 py-1 pr-4 pl-3 md:p-0">EN</a>
+                        {locale === 'pt' ? <Link href={asPath} locale={'en'}><a className="block ml-3 py-1 pr-4 pl-3 md:p-0">EN</a></Link> : <Link href={asPath} locale={'pt'}><a className="block ml-3 py-1 pr-4 pl-3 md:p-0">PT</a></Link>}
                         <div className='ml-3 flex justify-start items-center'>
                             <a href={socials.instagram} target="_blank" rel='noreferrer' className="block py-1 pr-1 pl-3 md:p-0"><RiInstagramFill/></a>
                             <a href={socials.facebook} target="_blank" rel='noreferrer' className="block py-1 pr-1 pl-3 md:p-0"><AiFillFacebook/></a>
--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ const nextConfig = {
     domains: ['www.datocms-assets.com'],
     minimumCacheTTL: 31536000,
   },
-  // i18n: {
-      // providing the locales supported by your application
-   // locales: ["en", "pt"],
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["en", "pt"],
     //  default locale used when the non-locale paths are visited
-   // defaultLocale: "pt",
-  //},
+    defaultLocale: "pt",
+  },
 }
 
 module.exports = nextConfig
